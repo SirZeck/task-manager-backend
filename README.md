f98f3e115445c6636bc76c1ac75b4f278c54d3cf
Aditya Raj <adithebest.ar@gmail.com>
2021-12-23 15:54:19 +0530

    updated README
    Signed-off-by: Aditya Raj <adithebest.ar@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Task Manager API
+
+```
+app.get('/api/v1/tasks')        - get all the tasks
+app.post('/api/v1/tasks')       - create a new task
+app.get('/api/v1/tasks/:id')    - get single task
+app.patch('/api/v1/tasks/:id')  - update task
+app.delete('/api/v1/tasks/:id') - delete task
+```
--- app.js
+++ app.js
@@ -15,12 +15,6 @@ app.use('/api/v1/tasks', tasks)
 app.use(notFound)
 app.use(errorHandlerMiddleware)
 
-// app.get('/api/v1/tasks')        - get all the tasks
-// app.post('/api/v1/tasks')       - create a new task
-// app.get('/api/v1/tasks/:id')    - get single task
-// app.patch('/api/v1/tasks/:id')  - update task
-// app.delete('/api/v1/tasks/:id') - delete task
-
 const port = process.env.PORT || 3000
 
 const start = async () => {
